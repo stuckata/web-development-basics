a73e0ba9f444776f78c59cdd0c3d2068e673aeec
stuckata <stu.kirkov@gmail.com>
2015-09-24 13:58:39 +0300

    problem 01

--- 02-State-Management/Homework/01-Guess-Number-Game/index.php
+++ 02-State-Management/Homework/01-Guess-Number-Game/index.php
@@ -1,11 +1,19 @@
 <h1>Guess Number Game</h1>
-<form action="" method="post">
+<form method="post">
     <input type="text" name="user" placeholder="Enter Your Name...">
-    <input type="button" id="submit" name="startGame" value="Start Game">
+    <input type="submit" id="submit" name="startGame" value="Start Game">
 </form>
 
 <?php
-if(isset($_POST['startGame']) && trim($_POST['user']) !== null){
-    $user = $_POST['user'];
+session_start();
+
+if(!isset($_SESSION['username'])){
+    $_SESSION['username'] = null;
+}
+
+if(isset($_POST['startGame']) && (trim($_POST['user']) != null)) {
+    $_SESSION['username'] = $_POST['user'];
+    $_SESSION['randomNumber'] = rand(1, 100);
     header("Location: play.php");
 }
+?>
--- 02-State-Management/Homework/01-Guess-Number-Game/play.php
+++ 02-State-Management/Homework/01-Guess-Number-Game/play.php
@@ -1,3 +1,34 @@
-<h1><?php 'Hello ' . $user ?></h1>
+<?php
+session_start();
+
+var_dump($_SESSION['randomNumber']);
+if(!isset($_SESSION['username'])){
+    header("Location: index.php");
+}
+?>
+
+<form method="post">
+    <h1>Enter Integer Number in range [1 - 100]</h1>
+    <input type="text" name="number" placeholder="Enter Your Guess Here...">
+    <input type="submit" name="submitNumber" value="Try">
+</form>
 
 <?php
+if(isset($_POST['submitNumber'])){
+
+    $number = intval($_POST['number']);
+
+    if(is_int($number) && $number > 0 && $number < 101){
+        if($number == $_SESSION['randomNumber']){
+            echo '<h1>Congratulations, ' . $_SESSION['username'] . '</h1><br>';
+            echo '<input type="submit" name="playAgain" value="Play Again">';
+        } else if($number < $_SESSION['randomNumber']){
+            echo '<h1>Down</h1>';
+        } else {
+            echo '<h1>Up</h1>';
+        }
+    } else {
+        echo 'Enter a valid number';
+    }
+}
+?>
